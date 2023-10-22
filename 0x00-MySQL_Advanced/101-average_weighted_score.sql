a832b0a03c173e90c366b9d252306707a6aaf3e0
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-10-22 15:47:31 +0000

    A stored procedure that computes and store the average weighted score for all students

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,14 @@
+-- script that creates a stored procedure that computes and store the average weighted score for all students.
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+BEGIN
+	UPDATE users
+	SET average_score = (
+        SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+        FROM corrections
+        INNER JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = users.id
+	);
+END
+$$
