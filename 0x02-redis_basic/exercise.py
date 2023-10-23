9e9682dc4b80a4b2cb236c9587edeeecb6108b68
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-10-23 18:45:37 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ A module that defines the class Cache """
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 import uuid
 
 
@@ -22,3 +22,43 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, int, float, None]:
+        """ Method that retrieves data from Redis. The callable will
+            be used to convert the data back to the desired format
+        """
+
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+
+        if fn is not None:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """ Method that will automatically parametrize Cache.get
+            with the correct conversion function str()
+        """
+
+        val = self.get(key)
+
+        if val is not None:
+            return str(val)
+        else:
+            return None
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """ Method that will automatically parametrize Cache.get
+            with the correct conversion function int()
+        """
+
+        value = self.get(key)
+
+        if value is not None:
+            return int(value)
+        else:
+            return None
